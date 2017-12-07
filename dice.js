2b202746b2b2530427f0c636bb8146779879b036
Jonah Snider <me@jonahsnider.ninja>
2017-12-07 08:39:53 -0800

    Add main bot file

---
+++ dice.js
@@ -0,0 +1,44 @@
+//Copyright 2017 Jonah Snider
+
+// Set up dependencies
+const {
+    CommandoClient
+} = require("discord.js-commando");
+const path = require("path");
+
+// Set up bot metadata
+const client = new CommandoClient({
+    commandPrefix: "$",
+    owner: ["210024244766179329"]
+});
+
+client.registry
+    // Registers your custom command groups
+    .registerGroups([
+        ["dice", "Dice"],
+        ["util", "Utility"],
+        ["dev", "Developer"]
+    ])
+
+    // Registers all built-in groups, commands, and argument types
+    .registerDefaults()
+
+    // Registers all of your commands in the ./commands/ directory
+    .registerCommandsIn(path.join(__dirname, "commands"));
+
+client.on("ready", () => {
+    console.log("Logged in!");
+
+    // Set game presence to the help command once loaded
+    client.user.setPresence({
+        game: {
+            name: "@Dice help",
+            type: 0
+        }
+    });
+});
+
+
+
+// Log in the bot
+client.login(process.env.BOT_TOKEN);
