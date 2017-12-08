325d4449cde488fe7e94d9a1382166d3f1621b61
Jonah Snider <me@jonahsnider.ninja>
2017-12-08 15:51:22 -0800

    Added maximum target multiplier checking

--- commands/util/calculator.js
+++ commands/util/calculator.js
@@ -10,7 +10,7 @@ module.exports = class WinCalculator extends Command {
             group: "util",
             memberName: "calculator",
             description: "Calculate the odds of winning a game.",
-            aliases: ["calc", "chance", "win-chance", "win-percentage", "percentage"],
+            aliases: ["calc", "chance", "win-chance", "win-percentage", "percentage", "percent"],
             examples: ["calculator"],
             args: [{
                 key: "multiplier",
@@ -24,10 +24,12 @@ module.exports = class WinCalculator extends Command {
     run(msg, { multiplier }) {
         if (multiplier < rules["minMultiplier"]) {
             return msg.reply(`Your target multiplier must be at least \`${rules["minMultiplier"]}\`.`);
+        } else if (multiplier > rules["maxMultiplier"]) {
+            return msg.reply(`Your target multiplier must be less than \`${rules["maxMultiplier"]}\`.`);
         }
 
         let winPercentage = Math.round((100 / multiplier) - rules["houseEdgePercentage"]);
         
-        return msg.reply(`Win Percentage: \`${winPercentage}%\`.`)
+        return msg.reply(`Win Percentage: \`${winPercentage}%\`.`);
     }
 };
