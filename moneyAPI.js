a7e70905b1ddd739bdfa91acdfeb6fd495de3f3d
Jonah Snider <me@jonahsnider.ninja>
2017-12-08 21:29:53 -0800

    Add API for calculating balances

---
+++ moneyAPI.js
@@ -0,0 +1,27 @@
+const rules = require("../../rules");
+const pathToJSON = `../../balances/${id}`;
+
+module.exports = function updateBalance(id, newBalance) {
+    // Load our target user file
+    
+
+    // Convert our data into JSON readable format
+    var balanceJSON = {
+        balance: newBalance
+    };
+    var profile = JSON.stringify(balanceJSON);
+
+    // Write the balance to a file
+    fs.writeFile(pathToJSON, profile);
+};
+
+module.exports = function getBalance(id) {
+    // Load our target user file
+    if (!fs.existsSync(`../../balances/${id}`)) {
+        // File doesn't exist, so make one
+        updateBalance(id, rules["newUserBalance"]);
+    }
+
+    balanceJSON = require(pathToJSON);
+    return balanceJSON["balance"];
+};
