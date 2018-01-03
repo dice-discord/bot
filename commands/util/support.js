509a7ce283e56bb82205d72c77a90c20f7b0a89b
Jonah Snider <me@jonahsnider.ninja>
2018-01-03 12:41:39 -0800

    Add support command

---
+++ commands/util/support.js
@@ -0,0 +1,21 @@
+const {
+    Command
+} = require("discord.js-commando");
+const rules = require("../../rules");
+
+module.exports = class SupportCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "support",
+            group: "util",
+            memberName: "support",
+            description: `An invite to the <@${rules[houseID]}> server`,
+            aliases: ["home", "report", "bug"],
+            examples: ["support"],
+        });
+    }
+
+    run(msg) {
+        msg.reply("👋 https://discord.gg/jTHpDv9");
+    }
+};
