18554409ac857b2ceaaf359c51e51c141c2904b9
Jonah Snider <me@jonahsnider.ninja>
2018-01-06 12:12:53 -0800

    Improve how list is sent

--- commands/dev/server-list.js
+++ commands/dev/server-list.js
@@ -1,36 +1,37 @@
-const {
-    Command
-} = require("discord.js-commando");
-const rules = require("../../rules");
+const { Command } = require('discord.js-commando');
+const rules = require('../../rules');
 
 module.exports = class ServerListCommand extends Command {
     constructor(client) {
         super(client, {
-            name: "server-list",
-            group: "dev",
-            memberName: "server-list",
-            description: `List all servers <@${rules["houseID"]}> is on`,
-            details: "Only the bot owner(s) may use this command.",
-            aliases: ["list-servers", "guild-list", "list-guilds"],
-            examples: ["server-list"],
+            name: 'server-list',
+            group: 'dev',
+            memberName: 'server-list',
+            description: `List all servers <@${rules.houseID}> is on`,
+            details: 'Only the bot owner(s) may use this command.',
+            aliases: ['list-servers', 'guild-list', 'list-guilds'],
+            examples: ['server-list'],
             throttling: {
                 usages: 2,
-                duration: 30
+                duration: 30,
             },
-            ownerOnly: true
+            ownerOnly: true,
         });
     }
 
     run(msg) {
-        const guilds = this.client.guilds;
-        let guildList = [];
-        for (let value of guilds.values()) {
-            guildList.push(`${value.name} (${value.id})`);
+        let guilds = this.client;
+        guilds = this.client.guilds;
+        const guildList = [];
+
+        for (const value of guilds.values()) {
+            guildList.push(`${value.name} (\`${value.id}\`)`);
         }
 
-        for (const item of guildList) {
+        /* for (const item of guildList) {
             msg.say(item);
-        }
+        }*/
+        msg.say(guildList.join('\n'));
 
         return msg.reply(`👥 Finished! ${guilds.size} servers in total.`);
     }
--- commands/dev/user-list.js
+++ commands/dev/user-list.js
@@ -1,46 +1,49 @@
-const {
-    Command
-} = require("discord.js-commando");
-const rules = require("../../rules");
-const diceAPI = require("../../diceAPI");
-const winston = require("winston");
+const { Command } = require('discord.js-commando');
+const rules = require('../../rules');
+const diceAPI = require('../../diceAPI');
+const winston = require('winston');
 
 module.exports = class UserListCommand extends Command {
     constructor(client) {
         super(client, {
-            name: "user-list",
-            group: "dev",
-            memberName: "user-list",
-            description: `List all users of <@${rules["houseID"]}>`,
-            details: "Only the bot owner(s) may use this command.",
-            aliases: ["list-users"],
-            examples: ["user-list"],
+            name: 'user-list',
+            group: 'dev',
+            memberName: 'user-list',
+            description: `List all users of <@${rules.houseID}>`,
+            details: 'Only the bot owner(s) may use this command.',
+            aliases: ['list-users'],
+            examples: ['user-list'],
             throttling: {
                 usages: 2,
-                duration: 30
+                duration: 30,
             },
-            ownerOnly: true
+            ownerOnly: true,
         });
     }
 
     async run(msg) {
         const database = await diceAPI.allUsers();
         const botClient = this.client;
+        const userList = [];
 
         async function userTagFromID(arrayPlace) {
-            const targetID = database[arrayPlace]["id"];
+            const targetID = database[arrayPlace].id;
             winston.debug(`Checking user tag from array index ${arrayPlace}`);
             if (botClient.users.get(targetID)) {
-                return await botClient.users.get(targetID).tag;
-            } else {
-                return await botClient.users.fetch(targetID).tag;
+                return botClient.users.get(targetID).tag;
+            }
+            else {
+                return botClient.users.fetch(targetID).tag;
             }
         }
 
-        for (let index = 0; index < database.length; index++) {
-            msg.say(`${await userTagFromID(index)} (${database[index]["id"]})`);
+        for (let index = 0; index < 50; index++) {
+            userList.push(`${await userTagFromID(index)} (\`${database[index].id}\`)`);
         }
 
-        return msg.reply(`👤 ${await diceAPI.totalUsers()} users in total.`);
+        winston.debug(`First item in userList: ${userList[0]}`);
+        msg.say(userList.join('\n'));
+
+        return msg.reply(`👤 ${await diceAPI.totalUsers()} users in total. Only 50 users were listed.`);
     }
 };
