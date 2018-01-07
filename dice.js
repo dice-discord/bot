6f133ba634db6eec7a733a3ebb3d13a0b3bae626
Jonah Snider <me@jonahsnider.ninja>
2018-01-07 15:55:54 -0800

    Fix how updateServerCount is called

--- dice.js
+++ dice.js
@@ -1,11 +1,14 @@
 // Copyright 2018 Jonah Snider
 
 // Set up dependencies
-const { CommandoClient } = require('discord.js-commando');
+const {
+    CommandoClient,
+} = require('discord.js-commando');
 const path = require('path');
 const winston = require('winston');
 winston.level = 'debug';
 const packageData = require('./package');
+const diceAPI = require('./diceAPI');
 const request = require('request');
 
 // Set up bot metadata
@@ -30,6 +33,89 @@ client.registry
     // Registers all of your commands in the ./commands/ directory
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
+// Update server counter on bot listings
+
+// Discordbots.org
+function sendDiscordBotsORGServerCount(serverData) {
+    const options = {
+        method: 'POST',
+        url: 'https://discordbots.org/api/bots/388191157869477888/stats',
+        headers: {
+            'cache-control': 'no-cache',
+            authorization: process.env.DISCORDBOTSORG_TOKEN,
+        },
+        body: {
+            server_count: serverData,
+        },
+        json: true,
+    };
+
+    winston.debug(`discordbots.org token: ${process.env.DISCORDBOTSORG_TOKEN}`);
+    request(options, (err, httpResponse, body) => {
+        if (err) return winston.error(err);
+        if (body) {
+            winston.debug('DiscordBots.org results:');
+            winston.debug(body);
+        }
+    });
+}
+
+// Bots.discord.pw
+function sendBotsDiscordPWServerCount(serverData) {
+    const options = {
+        method: 'POST',
+        url: 'https://bots.discord.pw/api/bots/388191157869477888/stats',
+        headers: {
+            'cache-control': 'no-cache',
+            authorization: process.env.BOTSDISCORDPW_TOKEN,
+        },
+        body: {
+            server_count: serverData,
+        },
+        json: true,
+    };
+
+    winston.debug(`bots.discord.pw token: ${process.env.BOTSDISCORDPW_TOKEN}`);
+    request(options, (err, httpResponse, body) => {
+        if (err) return winston.error(err);
+        if (body) {
+            winston.debug('Bots.Discord.pw results:');
+            winston.debug(body);
+        }
+    });
+}
+
+// Bots.discordlist.net
+function sendDiscordlistServerCount(serverData) {
+    winston.debug(`bots.discordlist.net token: ${process.env.DISCORDLIST_TOKEN}`);
+    request({
+        url: 'https://bots.discordlist.net/api',
+        method: 'POST',
+        json: true,
+        body: {
+            servers: serverData,
+            token: process.env.DISCORDLIST_TOKEN,
+        },
+    }, (err, httpResponse, body) => {
+        if (err) return winston.error(err);
+        if (body) {
+            winston.debug('Discordlist results:');
+            winston.debug(body);
+        }
+    });
+}
+
+// Run all three at once
+function updateServerCount(serverData) {
+    if (client.user.id === '388191157869477888') {
+        winston.verbose('Sending POST requests to bot listings.');
+        sendDiscordBotsORGServerCount(serverData);
+        sendBotsDiscordPWServerCount(serverData);
+        sendDiscordlistServerCount(serverData);
+    }
+}
+// module.exports.updateServerCount = updateServerCount;
+
 client.on('ready', () => {
     winston.info('Logged in!');
     winston.verbose(`Node.js version: ${process.version}`);
@@ -40,100 +126,41 @@ client.on('ready', () => {
         type: 'WATCHING',
     });
 
-    // Update server counter on bot listings
-
-    // Run all three at once
-    function updateServerCount() {
-        if (client.user.id === '388191157869477888') {
-            winston.verbose('Sending POST requests to bot listings.');
-            sendDiscordBotsORGServerCount();
-            sendBotsDiscordPWServerCount();
-            sendDiscordlistServerCount();
-        }
-    }
-    module.exports.updateServerCount = updateServerCount;
-
-    // Discordbots.org
-    function sendDiscordBotsORGServerCount() {
-        const options = {
-            method: 'POST',
-            url: 'https://discordbots.org/api/bots/388191157869477888/stats',
-            headers: {
-                'cache-control': 'no-cache',
-                authorization: process.env.DISCORDBOTSORG_TOKEN,
-            },
-            body: {
-                server_count: client.guilds.size,
-            },
-            json: true,
-        };
-
-        winston.debug(`discordbots.org token: ${process.env.DISCORDBOTSORG_TOKEN}`);
-        request(options, (err, httpResponse, body) => {
-            if (err) return winston.error(err);
-            if (body) {
-                winston.debug('DiscordBots.org results:');
-                winston.debug(body);
-            }
-        });
-    }
-
-    // Bots.discord.pw
-    function sendBotsDiscordPWServerCount() {
-        const options = {
-            method: 'POST',
-            url: 'https://bots.discord.pw/api/bots/388191157869477888/stats',
-            headers: {
-                'cache-control': 'no-cache',
-                authorization: process.env.BOTSDISCORDPW_TOKEN,
-            },
-            body: {
-                server_count: client.guilds.size,
-            },
-            json: true,
-        };
-
-        winston.debug(`bots.discord.pw token: ${process.env.BOTSDISCORDPW_TOKEN}`);
-        request(options, (err, httpResponse, body) => {
-            if (err) return winston.error(err);
-            if (body) {
-                winston.debug('Bots.Discord.pw results:');
-                winston.debug(body);
-            }
-        });
-    }
-
-    // Bots.discordlist.net
-    function sendDiscordlistServerCount() {
-        winston.debug(`bots.discordlist.net token: ${process.env.DISCORDLIST_TOKEN}`);
-        request({
-            url: 'https://bots.discordlist.net/api',
-            method: 'POST',
-            json: true,
-            body: {
-                servers: client.guilds.size,
-                token: process.env.DISCORDLIST_TOKEN,
-            },
-        }, (err, httpResponse, body) => {
-            if (err) return winston.error(err);
-            if (body) {
-                winston.debug('Discordlist results:');
-                winston.debug(body);
-            }
-        });
-    }
-
-    updateServerCount();
+    updateServerCount(client.guilds.size);
 });
 
 client.on('guildCreate', () => {
     // Bot joins a new server
-    this.updateServerCount();
+    updateServerCount(client.guilds.size);
 });
 
 client.on('guildDelete', () => {
     // Bot leaves a server
-    this.updateServerCount();
+    updateServerCount(client.guilds.size);
+});
+
+client.on('message', (msg) => {
+    if (msg.content.startsWith(client.commandPrefix)) {
+        // Command logger for research purposes
+        const channel = client.channels.find('id', '399458745480118272');
+        channel.send({
+            embed: {
+                'author': {
+                    'name': msg.author.tag,
+                    'icon_url': msg.author.displayAvatarURL(128),
+                },
+                'timestamp': new Date(msg.createdTimestamp),
+                'fields': [{
+                    'name': 'Message',
+                    'value': msg.content,
+                }, {
+                    'name': 'Balance',
+                    'value': diceAPI.getBalance(msg.author.id),
+                },
+                ],
+            },
+        });
+    }
 });
 
 // Log in the bot
