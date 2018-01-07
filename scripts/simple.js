ac6c1208dc4c9fb9149116a688c3bf00aa8e3039
Jonah Snider <me@jonahsnider.ninja>
2018-01-06 21:39:23 -0800

    Improve formatting

--- scripts/simple.js
+++ scripts/simple.js
@@ -1,24 +1,24 @@
 // Put your token here:
 // It should look something like this:
-const token = "mfa.RZ7ZFgfBwZ9ISK-lwLhVDLAMQVkIWTTq-AdyVY-iR82XLx-pxAMhJ0zPldq6x7JyHK1Qc_vJAtVr8Nj4O7BH";
+const token = 'mfa.RZ7ZFgfBwZ9ISK-lwLhVDLAMQVkIWTTq-AdyVY-iR82XLx-pxAMhJ0zPldq6x7JyHK1Qc_vJAtVr8Nj4O7BH';
 
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
 // Default starting balance
 let balance = 100;
 let channel;
 // Create an instance of a Discord client
 const client = new Discord.Client();
-client.on("ready", () => {
+client.on('ready', () => {
     function play() {
         channel.send(`$play ${balance} 1.01`);
         // Calculate new balance if you won (doesn't check if you lost)
         balance = balance * 1.01;
-        console.log("Finished game! New balance: " + balance);
+        console.log('Finished game! New balance: ' + balance);
     }
-    console.log("I am ready!");
+    console.log('I am ready!');
     // Find game channel
-    channel = client.channels.find("id", "389228857602867220");
+    channel = client.channels.find('id', '389228857602867220');
     // Every second, play a non risky game with entire balance
     setInterval(play, 2000);
 });
