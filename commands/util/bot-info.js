f55c55117421f69d4bc1b573c112edc9472472ff
Jonah Snider <me@jonahsnider.ninja>
2018-01-23 21:42:55 -0800

    Add more information

--- commands/util/bot-info.js
+++ commands/util/bot-info.js
@@ -19,11 +19,27 @@ module.exports = class BotInfoCommand extends Command {
 				color: 0x4caf50,
 				description: `${
 					this.client.user
-				} was made by <@210024244766179329> based off the game [bustadice](https://bustadice.com)`,
+				} was made by <@210024244766179329> based off the game [bustadice](https://bustadice.com).`,
 				thumbnail: {
 					url:
 						'https://cdn.discordapp.com/avatars/388191157869477888/6433e08d1796afb6efa1d37f8619f635.webp',
 				},
+				fields: [
+					{
+						name: 'Discord.js Version',
+						value: '12.0.0-dev',
+						inline: true,
+					},
+					{
+						name: 'Discord.js Commando',
+						value: '0.9.0',
+						inline: true,
+					},
+					{
+						name: 'Support team',
+						value: '<@115511836543025152> and <@208970190547976202>',
+					},
+				],
 			},
 		});
 	}
