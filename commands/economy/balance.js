036f5460a44c1031557ea9a22e3258a2d5cdc736
Jonah Snider <me@jonahsnider.ninja>
2018-02-09 08:25:44 -0800

    Improve formatting

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -8,13 +8,11 @@ module.exports = class BalanceCommand extends Command {
 			name: 'balance',
 			group: 'economy',
 			memberName: 'balance',
-
 			description: 'Check a user\'s balance',
 			aliases: ['bal', 'balance-check', 'bal-check', 'credits'],
 			examples: ['balance', 'balance @PizzaFox#0075'],
 			args: [{
 				key: 'user',
-
 				prompt: 'Who\'s balance do you want to check?',
 				type: 'user',
 				default: ''
@@ -32,7 +30,6 @@ module.exports = class BalanceCommand extends Command {
 
 		// Bot checking
 		if (user.bot && user.id !== rules.houseID) {
-
 			return msg.reply('❌ Bots can\'t play.');
 		}
 
@@ -41,10 +38,8 @@ module.exports = class BalanceCommand extends Command {
 
 			// Someone else's balance
 			if (houseBalance < userBalance && user.id !== rules.houseID) {
-
 				return msg.reply(`🏦 ${user.tag}'s account has a balance of \`${userBalance}\` ${rules.currencyPlural}. That's more than ${this.client.user}!`);
 			} else {
-
 				return msg.reply(`🏦 ${user.tag}'s account has a balance of \`${userBalance}\` ${rules.currencyPlural}.`);
 			}
 		} else {
@@ -52,7 +47,6 @@ module.exports = class BalanceCommand extends Command {
 
 			// We are looking up the message author's balance
 			if (houseBalance < userBalance && user.id !== rules.houseID) {
-
 				return msg.reply(`🏦 You have a balance of \`${userBalance}\` ${rules.currencyPlural}. That's more than ${this.client.user}!`);
 			} else {
 				return msg.reply(`🏦 You have a balance of \`${userBalance}\` ${rules.currencyPlural}.`);
