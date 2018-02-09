27a294159c86fe1641dd7b090fda441d63d52638
Jonah Snider <me@jonahsnider.ninja>
2018-02-09 08:24:44 -0800

    Use Commando type checking/validation

--- commands/util/calculator.js
+++ commands/util/calculator.js
@@ -16,25 +16,17 @@ module.exports = class CalculatorCommand extends Command {
 					key: 'multiplier',
 					label: 'multiplier',
 					prompt: 'How much do you want to multiply your wager by?',
-					type: 'string',
+					type: 'float',
 					// Round multiplier to second decimal place
 					parse: multiplierString => diceAPI.simpleFormat(multiplierString),
+					min: rules.minMultiplier,
+					max: rules.maxMultiplier
 				},
 			],
 		});
 	}
 
 	run(msg, { multiplier }) {
-		if (multiplier < rules.minMultiplier) {
-			return msg.reply(`❌ Your target multiplier must be at least \`${rules.minMultiplier}\`.`);
-		} else if (multiplier > rules.maxMultiplier) {
-			return msg.reply(`❌ Your target multiplier must be less than \`${rules.maxMultiplier}\`.`);
-		} else if (isNaN(multiplier)) {
-			return msg.reply(`❌ \`${multiplier}\` is not a valid number.`);
-		}
-
-		return msg.reply(
-			`🔢 Win Percentage: \`${diceAPI.simpleFormat(diceAPI.winPercentage(multiplier))}%\`.`
-		);
+		return msg.reply(`🔢 Win Percentage: \`${diceAPI.simpleFormat(diceAPI.winPercentage(multiplier))}%\`.`);
 	}
 };
