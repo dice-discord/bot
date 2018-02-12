8273b237fbcac5486c5500b278bf87aa06dcf270
Jonah Snider <me@jonahsnider.ninja>
2018-02-11 21:47:56 -0800

    Add choose command

---
+++ commands/util/choose.js
@@ -0,0 +1,31 @@
+// Copyright Jonah Snider 2018
+
+const { Command } = require('discord.js-commando');
+
+module.exports = class ChooseCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'choose',
+			aliases: ['select', 'pick'],
+			group: 'util',
+			memberName: 'choose',
+			description: 'Choose something from a list you provide.',
+			examples: ['choose red | blue | yellow | green'],
+			args: [{
+				key: 'options',
+				prompt: 'What do you want to select?',
+				type: 'string',
+				infinite: true
+			}]
+		});
+	}
+
+	run(msg, { options }) {
+		// Argument checking
+		if (options.length < 2) return msg.reply('❌ Please provide 2 or more options.');
+
+		const randomNumber = Math.floor(Math.random() * (options.length - 0) + 0);
+
+		return msg.reply(`I choose #${randomNumber + 1}, ${options[randomNumber]}.`);
+	}
+};
