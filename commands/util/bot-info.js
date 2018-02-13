5a9949f55cda0ffe66d462adbd3da24412572a4e
Jonah Snider <me@jonahsnider.ninja>
2018-02-13 09:07:06 -0800

    Add typing

--- commands/util/bot-info.js
+++ commands/util/bot-info.js
@@ -18,43 +18,48 @@ module.exports = class BotInfoCommand extends Command {
 	}
 
 	run(msg) {
-		return msg.replyEmbed({
-			title: 'Dice',
-			url: 'https://github.com/PizzaFox/dice',
-			color: 0x4caf50,
-			description: `${this.client.user} was made by <@210024244766179329> based off the game [bustadice](https://bustadice.com).`,
-			thumbnail: {
-				url: this.client.user.displayAvatarURL({ format: 'webp' })
-			},
-			fields: [{
-				name: '🕒 Uptime',
-				value: moment.duration(this.client.uptime).humanize(),
-				inline: true
-			}, {
-				name: '🎲 Dice Version',
-				value: `v${packageData.version}`,
-				inline: true
-			},
-			{
-				name: '🔧 GitHub',
-				value: `${this.client.user} is open source! [See the repository](https://github.com/PizzaFox/dice).`,
-				inline: true
-			},
-			{
-				name: '🔧 Discord.js Version',
-				value: '12.0.0-dev',
-				inline: true
-			},
-			{
-				name: '🤖 Discord.js Commando Version',
-				value: '0.9.0',
-				inline: true
-			},
-			{
-				name: '🤠 Support team',
-				value: '<@115511836543025152> and <@208970190547976202>'
-			}
-			]
-		});
+		try {
+			msg.channel.startTyping();
+			return msg.replyEmbed({
+				title: 'Dice',
+				url: 'https://github.com/PizzaFox/dice',
+				color: 0x4caf50,
+				description: `${this.client.user} was made by <@210024244766179329> based off the game [bustadice](https://bustadice.com).`,
+				thumbnail: {
+					url: this.client.user.displayAvatarURL({ format: 'webp' })
+				},
+				fields: [{
+						name: '🕒 Uptime',
+						value: moment.duration(this.client.uptime).humanize(),
+						inline: true
+					}, {
+						name: '🎲 Dice Version',
+						value: `v${packageData.version}`,
+						inline: true
+					},
+					{
+						name: '🔧 GitHub',
+						value: `${this.client.user} is open source! [See the repository](https://github.com/PizzaFox/dice).`,
+						inline: true
+					},
+					{
+						name: '🔧 Discord.js Version',
+						value: '12.0.0-dev',
+						inline: true
+					},
+					{
+						name: '🤖 Discord.js Commando Version',
+						value: '0.9.0',
+						inline: true
+					},
+					{
+						name: '🤠 Support team',
+						value: '<@115511836543025152> and <@208970190547976202>'
+					}
+				]
+			});
+		} finally {
+			msg.channel.stopTyping();
+		}
 	}
 };
