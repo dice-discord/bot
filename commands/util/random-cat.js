f58086843f593ab22bb6124d28a4abcd20cc6218
Jonah Snider <me@jonahsnider.ninja>
2018-02-13 16:35:22 -0800

    Create random-cat.js

---
+++ commands/util/random-cat.js
@@ -0,0 +1,46 @@
+// Copyright 2018 Jonah Snider
+
+const { Command } = require('discord.js-commando');
+const rp = require('request-promise');
+const replaceall = require('replaceall');
+
+module.exports = class RandomCatImageCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'random-cat-image',
+			group: 'util',
+			memberName: 'random-cat-image',
+			description: 'Get a picture of a random cat',
+			aliases: ['random-cat', 'cat-image', 'cat'],
+			examples: ['cat-image', 'random-cat pc'],
+			throttling: {
+				usages: 1,
+				duration: 5
+			}
+		});
+	}
+
+	async run(msg) {
+		try {
+			msg.channel.startTyping();
+
+			const options = {
+				uri: 'http://random.cat/meow',
+				json: true
+			};
+			const result = await rp(options);
+
+			if (!result.file) {
+				return msg.reply('❌ There was an error with the API we use (http://random.cat).');
+			}
+
+			return msg.replyEmbed({
+				url: 'http://random.cat',
+				image: { url: result.file }
+			});
+		} finally {
+			msg.channel.stopTyping();
+		}
+
+	}
+};
