28c4f38bb5ce6f408e447a2f6f1d013fa733ba4a
Jonah Snider <me@jonahsnider.ninja>
2018-02-20 00:44:17 -0800

    Add russian roulette command

---
+++ commands/games/russian-roulette.js
@@ -0,0 +1,29 @@
+// Copyright 2018 Jonah Snider
+
+const { Command } = require('discord.js-commando');
+
+module.exports = class RussianRouletteCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'russian-roulette',
+			group: 'games',
+			memberName: 'russian-roulette',
+			description: 'Play a game of Russian roulette',
+			throttling: {
+				usages: 1,
+				duration: 3
+			}
+		});
+	}
+
+	run(msg) {
+		// Round numbers to second decimal place
+		const randomNumber = Math.round(Math.random() * (6 - 1) + 1);
+
+		if (randomNumber === 6) {
+			return msg.reply('💥 *Bang*. You lose.');
+		}
+
+		return msg.reply('🔫 *click*. You survived');
+	}
+};
