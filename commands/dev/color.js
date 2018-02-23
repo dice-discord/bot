0fbf09dcb1414c2c937f4563234bd31be8ebf481
Jonah Snider <me@jonahsnider.ninja>
2018-02-22 23:11:17 -0800

    Add color command

---
+++ commands/dev/color.js
@@ -0,0 +1,60 @@
+// Copyright 2018 Jonah Snider
+
+const { Command } = require('discord.js-commando');
+const { Util } = require('discord.js');
+const parseColor = require('parse-color');
+
+module.exports = class ColorCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'color',
+			aliases: ['rbg', 'cmyk', 'hsv', 'hsl', 'hex', 'hexadecimal', 'colors'],
+			group: 'dev',
+			memberName: 'color',
+			description: 'Display and convert a color',
+			details: 'Display and convert a color from and to hexadecimal, HSL, RGB, CMYK, and CSS name',
+			examples: ['color blue', 'color #deaded', 'color hsl(210,50,50)'],
+			args: [{
+				key: 'color',
+				prompt: 'What color do you want to get information on?',
+				type: 'string',
+				parse: input => parseColor(input)
+			}],
+			throttling: {
+				usages: 2,
+				duration: 60
+			}
+		});
+	}
+
+	run(msg, { color }) {
+		if (!color.cmyk || !color.rgb || !color.hsv || !color.hsl || !color.hex) {
+			return msg.reply('❌ Invalid color.');
+		}
+
+		return msg.replyEmbed({
+			color: Util.resolveColor(color.rgb),
+			fields: [{
+				name: 'CSS Keyword',
+				value: color.keyword || 'None'
+			},
+			{
+				name: 'Hexadecimal',
+				value: color.hex.toString()
+			}, {
+				name: 'CMYK',
+				value: color.cmyk.join(', ')
+			}, {
+				name: 'HSL',
+				value: color.hsl.join(', ')
+			}, {
+				name: 'HSV',
+				value: color.hsv.join(', ')
+			}, {
+				name: 'RGB',
+				value: color.rgb.join(', ')
+			}
+			]
+		});
+	}
+};
