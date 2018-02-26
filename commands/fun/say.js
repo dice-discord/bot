36a0ce0b36062e7472a815eae30dc672da98545d
Jonah Snider <me@jonahsnider.ninja>
2018-02-25 20:42:03 -0800

    Add say command

---
+++ commands/fun/say.js
@@ -0,0 +1,32 @@
+// Copyright 2018 Jonah Snider
+
+const { Command } = require('discord.js-commando');
+
+module.exports = class SayCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'say',
+			group: 'fun',
+			memberName: 'say',
+			description: 'Have the bot say a phrase you specify',
+			details: 'Only the bot owner(s) may use this command',
+			aliases: ['repeat'],
+			examples: ['say I am a bot'],
+			ownerOnly: true,
+			args: [{
+				key: 'phrase',
+				prompt: 'What do you want to have said?',
+				type: 'string'
+			}],
+			throttling: {
+				usages: 2,
+				duration: 6
+			}
+		});
+	}
+
+	async run(msg, { phrase }) {
+		msg.delete();
+		return msg.say(phrase);
+	}
+};
