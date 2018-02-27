e6fe72f124c51a59979aa302228323eef72d93a5
Jonah Snider <me@jonahsnider.ninja>
2018-02-26 16:46:13 -0800

    Add number averaging command

---
+++ commands/util/average-numbers.js
@@ -0,0 +1,35 @@
+// Copyright 2018 Jonah Snider
+
+const { Command } = require('discord.js-commando');
+
+module.exports = class AverageNumbersCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'average-numbers',
+			aliases: ['average', 'avg-numbers', 'avg'],
+			group: 'util',
+			memberName: 'average-numbers',
+			description: 'Gets the average of several numbers',
+			examples: ['average 55 59 45 61', 'average 5.01 1.01 -8.04 15.067'],
+			args: [{
+				key: 'numbers',
+				prompt: 'What\'s another number you want to be averaged?',
+				type: 'float',
+				label: 'number',
+				infinite: true
+			}],
+			throttling: {
+				usages: 2,
+				duration: 6
+			}
+		});
+	}
+
+	run(msg, { numbers }) {
+		// Argument checking
+		if (numbers.length < 2) return msg.reply('❌ Please provide 2 or more numbers.');
+
+		const reducer = (accumulator, currentValue) => accumulator + currentValue;
+		return msg.reply(`🔢 The average is ${numbers.reduce(reducer) / numbers.length}.`);
+	}
+};
