b392ea8f73f7b55fff32c557ba9b0bcdc8fd4956
Jonah Snider <me@jonahsnider.ninja>
2018-03-20 16:14:45 -0700

    Add roll die command

---
+++ commands/fun/roll-die.js
@@ -0,0 +1,34 @@
+// Copyright 2018 Jonah Snider
+
+const { Command } = require('discord.js-commando');
+
+module.exports = class RollDieCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'roll-die',
+			aliases: ['roll-dice', 'die', 'dice'],
+			group: 'fun',
+			memberName: 'roll-die',
+			description: 'Roll a die ',
+			examples: ['roll-die', 'roll-die 20'],
+			args: [{
+				key: 'sides',
+				prompt: 'How many sides do you want your die to have?',
+				type: 'integer',
+				label: 'number of die sides',
+				default: 6,
+				min: 1
+			}],
+			throttling: {
+				usages: 2,
+				duration: 6
+			}
+		});
+	}
+
+	run(msg, { sides }) {
+		const randomNumber = Math.floor(Math.random() * sides);
+
+		return msg.reply(`🎲 You rolled a ${randomNumber}.`);
+	}
+};
