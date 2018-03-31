1db68296077ee9e4923f4f2b02527bb6a6cc8a1a
Jonah Snider <me@jonahsnider.ninja>
2018-03-31 08:47:20 -0700

    Link commands to their page

--- commands/dev/generate-command-list.js
+++ commands/dev/generate-command-list.js
@@ -24,6 +24,6 @@ module.exports = class GenerateCommandListCommand extends Command {
 
 		msg.say(groups.filter(grp => grp.commands)
 		// eslint-disable-next-line max-len
-		.map(grp => `${grp.commands.filter(cmd => !cmd.ownerOnly).map(cmd => `|\\\`${cmd.name}\\\`|${cmd.description}|${cmd.group.name}|`).join('\n')}`), { split: true });
+		.map(grp => `${grp.commands.filter(cmd => !cmd.ownerOnly).map(cmd => `|[\\\`${cmd.name}\\\`](/commands/${cmd.group.name.toLowerCase()}/${cmd.name})|${cmd.description}|${cmd.group.name}|`).join('\n')}`), { split: true });
 	}
 };
