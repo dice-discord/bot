fccc087c13c140011e9e29db8815dd856c6e900c
Jonah Snider <me@jonahsnider.ninja>
2018-04-01 10:16:30 -0700

    Add LMGTFY command

---
+++ commands/search/lmgtfy.js
@@ -0,0 +1,31 @@
+// Copyright Jonah Snider 2018
+
+const { Command } = require('discord.js-commando');
+
+module.exports = class LMGTFYCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'lmgtfy',
+			group: 'search',
+			memberName: 'lmgtfy',
+			description: 'Generate a let-me-Google-that-for-you link.',
+			examples: ['look-up-stock AAPL'],
+			aliases: ['let-me-google-that-for-you'],
+			throttling: {
+				usages: 2,
+				duration: 6
+			},
+			args: [{
+				key: 'query',
+				prompt: 'What do you want the link to search for?',
+				type: 'string',
+				max: 500,
+				parse: value => encodeURIComponent(value)
+			}]
+		});
+	}
+
+	run(msg, { query }) {
+		return msg.reply(`https://lmgtfy.com/?iie=1&q=${query}`);
+	}
+};
