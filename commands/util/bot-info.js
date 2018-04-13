bcc0aa91c1b13ea63c2ffcc53caa5b9b32c11d26
Jonah Snider <me@jonahsnider.ninja>
2018-04-13 02:19:57 -0700

    Add RAM usage

--- commands/util/bot-info.js
+++ commands/util/bot-info.js
@@ -12,7 +12,17 @@ module.exports = class BotInfoCommand extends Command {
 			group: 'util',
 			memberName: 'bot-info',
 			description: `Information about <@${config.clientID}>.`,
-			aliases: ['github', 'uptime', 'library', 'git', 'lib', 'version', 'bot'],
+			aliases: [
+				'github',
+				'uptime',
+				'git',
+				'version',
+				'bot',
+				'memory',
+				'ram',
+				'memory-usage',
+				'ram-usage'
+			],
 			clientPermissions: ['EMBED_LINKS'],
 			throttling: {
 				usages: 3,
@@ -31,36 +41,26 @@ module.exports = class BotInfoCommand extends Command {
 				// eslint-disable-next-line max-len
 				description: `${this.client.user} was made by <@210024244766179329>. It was first a game bot based off the game [bustadice](https://bustadice.com). Later more features were created and added, one by one creating the ${this.client.user} we have today. ${this.client.user}. In March 2018 Dice was accepted into the [Discoin](https://discoin.gitbooks.io/docs/content/users-guide.html) network, a system allowing for participating bots to convert currencies.`,
 				thumbnail: { url: this.client.user.displayAvatarURL({ format: 'webp' }) },
-				fields: [
-					{
-						name: '🕒 Uptime',
-						value: moment.duration(this.client.uptime).humanize(),
-						inline: true
-					}, {
-						name: '🎲 Dice Version',
-						value: `v${packageData.version}`,
-						inline: true
-					},
-					{
-						name: '🔧 GitHub',
-						value: `${this.client.user} is open source! [See the repository](https://github.com/PizzaFox/dice).`,
-						inline: true
-					},
-					{
-						name: '⚙ Discord.js Version',
-						value: '12.0.0-dev',
-						inline: true
-					},
-					{
-						name: '🤖 Discord.js Commando Version',
-						value: '0.9.0',
-						inline: true
-					},
-					{
-						name: '🤠 Support team',
-						value: 'okthx#1013 and Mr.Pig McOinks#3425'
-					}
-				]
+				fields: [{
+					name: '🕒 Uptime',
+					value: moment.duration(this.client.uptime).humanize(),
+					inline: true
+				}, {
+					name: '🎲 Dice version',
+					value: `v${packageData.version}`,
+					inline: true
+				}, {
+					name: '🔧 GitHub',
+					value: `${this.client.user} is open source! [See the repository](https://github.com/PizzaFox/dice).`,
+					inline: true
+				}, {
+					name: '🤠 Support team',
+					value: 'okthx#1013 and Mr.Pig McOinks#3425'
+				}, {
+					name: '⚙ RAM usage',
+					value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} megabytes`,
+					inline: true
+				}]
 			});
 		} finally {
 			msg.channel.stopTyping();
