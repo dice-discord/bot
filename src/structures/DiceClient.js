b4a8167dca388d178a23539be57a6713aa74c2bc
Jonah Snider <me@jonahsnider.ninja>
2018-11-14 21:36:13 -0800

    fix: Add error handling for StatsD

--- src/structures/DiceClient.js
+++ src/structures/DiceClient.js
@@ -1,6 +1,7 @@
 const { CommandoClient } = require('discord.js-commando');
 const database = require('../util/database');
 const StatsD = require('hot-shots');
+const logger = require('../util/logger');
 
 class DiceClient extends CommandoClient {
   constructor(options) {
@@ -9,6 +10,10 @@ class DiceClient extends CommandoClient {
     this.db = database;
     this.blacklist = [];
     this.stats = new StatsD();
+
+    this.stats.socket.on('error', error => {
+      logger.error('Error in socket: ', error);
+    });
   }
 }
 
