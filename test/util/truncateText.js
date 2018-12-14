11949154296cd8e7b2367198dc211f9383b7ee12
Jonah Snider <me@jonahsnider.ninja>
2018-12-13 21:12:43 -0800

    test: Use TAP

---
+++ test/util/truncateText.js
@@ -0,0 +1,4 @@
+const tap = require('tap');
+const truncateText = require('../../src/util/truncateText');
+
+tap.equal(truncateText('short string', 20), 'short string', 'doesn\'t truncate already short text');
--- test/util/truncateText.test.js
+++
@@ -1,6 +0,0 @@
-/* global test expect */
-const truncateText = require('../../src/util/truncateText');
-
-test('doesn\'t truncate already short text', () => {
-  expect(truncateText('string', 20)).toBe('string');
-});
