ccec327ab586c722226b2e6474968e16f3685f5e
Jonah Snider <jonah@jonah.pw>
2019-12-01 11:28:27 -0800

    feat: support Murder Mystery Bot on Discoin
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/config.js
+++ src/config.js
@@ -65,7 +65,7 @@ const config = {
     "zar"
   ],
   dblWebhookVerification: process.env.DBL_WEBHOOK_VERIFICATION,
-  discoinCurrencyCodes: ["dts", "elt", "kek", "rbn", "pic", "pbc"],
+  discoinCurrencyCodes: ["dts", "elt", "kek", "rbn", "pic", "pbc", "mmb"],
   discoinToken: process.env.DISCOIN_TOKEN,
   discordToken: process.env.DISCORD_TOKEN,
   fortniteTrackerNetworkToken: process.env.FORTNITE_TRACKER_NETWORK_TOKEN,
