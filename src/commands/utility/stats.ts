3a23a2dc1807875189e48e77ec6a0ae0314fe8c6
Jonah Snider <jonah@jonah.pw>
2020-05-11 18:44:55 -0700

    fix(commands): fix stats command output when clusters are starting
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/commands/utility/stats.ts
+++ src/commands/utility/stats.ts
@@ -1,5 +1,6 @@
 import {Message, MessageEmbed} from 'discord.js';
 import {DiceCommand, DiceCommandCategories} from '../../structures/DiceCommand';
+import {getClusterCount} from '../../util/shard';
 
 export default class StatsCommand extends DiceCommand {
 	constructor() {
@@ -17,7 +18,24 @@ export default class StatsCommand extends DiceCommand {
 		if (this.client.shard) {
 			const shardServerCounts = await (this.client.shard.broadcastEval('this.guilds.cache.size') as Promise<number[]>);
 
+			const clusterCount = getClusterCount(this.client.shard);
+
+			const actual = shardServerCounts.length;
+
 			serverCount = shardServerCounts.reduce((previous, value) => previous + value, 0);
+
+			if (actual !== clusterCount) {
+				/** The number of clusters that didn't respond. */
+				const unresponsiveClusterCount = Math.abs(clusterCount - actual);
+
+				return message.util?.send(
+					[
+						`It looks like ${unresponsiveClusterCount.toLocaleString()} clusters didn't respond`,
+						`The approximate server count is ${serverCount + unresponsiveClusterCount * 1000}`,
+						'To get the exact server count, please try this again in a bit once some more clusters have come online'
+					].join('\n')
+				);
+			}
 		}
 
 		return message.util?.send(
