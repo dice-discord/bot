d48af60fdd2ca3defb16985e983ad507d370d61d
Jonah Snider <jonah@jonah.pw>
2020-11-25 17:29:06 -0800

    test: properly ignore `tsc_output` from Jest
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- jest.config.js
+++ jest.config.js
@@ -144,7 +144,7 @@ module.exports = {
 	// ],
 
 	// An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-	testPathIgnorePatterns: ['/node_modules/', 'tsc_output']
+	testPathIgnorePatterns: ['/node_modules/', 'tsc_output/.*']
 
 	// The regexp pattern or array of patterns that Jest uses to detect test files
 	// testRegex: [/src\/.+\/.+\.ts/]
